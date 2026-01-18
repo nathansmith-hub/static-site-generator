d0807f394348a3f71d72039dc82e1a1c0604e47b
nathansmith-hub <nathanhsmith@live.com>
2026-01-18 16:28:16 +1300

    added textnode testing

---
+++ src/test_textnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_with_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is also a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_type(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_with_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot2.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_with_none_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
